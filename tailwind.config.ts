d89a2f67f6ccbd41a47c8f7dd7caa23e51e87b36
Maria Filtsova <filtsovamaria@seznam.cz>
2023-09-01 16:30:58 +0200

    chore: set up tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,31 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [],
+  purge: [
+    './public/index.html',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  content: {
+    files: [
+      './public/index.html',
+      './src/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    relative: true
+  },
   theme: {
     extend: {},
+    colors: {
+      focus: "#20A9F3",
+      hover: "#373737",
+      white: "#FFFFFF",
+      black: "#191919",
+      red: "#EF233C",
+      gray: {
+        50: "#808080",
+        80: "#CCCCCC",
+      },
+      bg: "#EEEFF0"
+    }
   },
   plugins: [],
 } satisfies Config;
