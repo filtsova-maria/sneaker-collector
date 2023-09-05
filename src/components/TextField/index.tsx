1e29c19bf57c077058c4da6948f226d5307e6dba
Maria Filtsova <filtsovamaria@seznam.cz>
2023-09-05 23:55:40 +0200

    feat: set up main page components

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,65 +1,14 @@
 import React from "react";
+import { IconType } from "react-icons/lib";
 
 interface Props {
-  inputName: boolean;
-  iconLeft: boolean;
-  iconRight: boolean;
-  state: "filling" | "active" | "filled" | "focus" | "empty" | "hover" | "alert";
-  className: any;
+  icon?: IconType;
+  prompt?: string;
+  className?: string;
 }
 
-export const TextField = ({ inputName = true, iconLeft, iconRight, state, className }: Props): JSX.Element => {
+export const TextField = ({ prompt, icon, className }: Props): JSX.Element => {
   return (
-    <div className={`text-field ${className}`}>
-      {inputName && (
-        <div className="label">
-          {state === "alert" && <>alert</>}
-
-          {state === "empty" && <>empty</>}
-
-          {state === "focus" && <>focus</>}
-
-          {state === "filled" && <>filled</>}
-
-          {state === "hover" && <>hover</>}
-
-          {state === "active" && <>active</>}
-
-          {state === "filling" && <>filling</>}
-        </div>
-      )}
-
-      {["active", "empty", "filled", "filling", "focus", "hover"].includes(state) && (
-        <div className={`text-input state-${state}`}>
-          <div className="MM-DD-YYYY">
-            {["empty", "filled", "focus", "hover"].includes(state) && <>Label</>}
-
-            {["active", "filling"].includes(state) && (
-              <>
-                <span className="span">
-                  {state === "active" && <>⏐</>}
-
-                  {state === "filling" && <>Label</>}
-                </span>
-                <span className="span-2">
-                  {state === "active" && <>Label</>}
-
-                  {state === "filling" && <>⏐</>}
-                </span>
-              </>
-            )}
-          </div>
-        </div>
-      )}
-
-      {state === "alert" && (
-        <>
-          <div className="MM-DD-YYYY-wrapper">
-            <div className="text-wrapper">Label</div>
-          </div>
-          <>{inputName && <div className="div">Alert message</div>}</>
-        </>
-      )}
-    </div>
+    <input></input>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,30 @@
+import { Button, TextField } from 'components'
 import React from 'react'
+import { MdAdd, MdSearch } from 'react-icons/md'
+import CollectionImage from 'assets/collection.svg';
 
 export default function Main() {
   return (
-    <div>Your collection</div>
+    <div>
+      <h1>Your collection</h1>
+      <TextField icon={MdSearch} />
+      <Button type='primary' iconPosition='left' icon={MdAdd}>Add new sneakers</Button>
+      {/* TODO: add and edit form drawers */}
+      {/* TODO: extract content to a different component */}
+      <div>
+        <img src={CollectionImage} alt="empty sneaker collection vector art" className='px-24 py-12 justify-self-center' />
+        <p>
+          Seem's like you still didn't add any new sneaker to your collection
+        </p>
+      </div>
+      {/* if items then generate list of item cards from data with sort header*/}
+      <div>
+        
+      </div>
+      {/* if search is not empty and no items then show not found*/}
+      <div>
+        
+      </div>
+    </div>
   )
 }
