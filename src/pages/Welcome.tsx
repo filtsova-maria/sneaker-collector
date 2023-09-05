4b3d97d5d7e1c3dd1b1fecf3916e9fa9299c7f30
Maria Filtsova <filtsovamaria@seznam.cz>
2023-09-06 00:02:26 +0200

    style: fix welcome image size

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -4,7 +4,7 @@ import { Link } from 'wouter';
 export default function Welcome() {
   return (
     <div className='grid grid-cols-[2fr_1.5fr]'>
-      <img src={WelcomeImage} alt="Welcome vector art" className='px-24 py-12 justify-self-center'/>
+      <img src={WelcomeImage} alt="Welcome vector art" className='px-24 py-12 justify-self-center min-w-[700px] self-center'/>
       <div className='self-center p-24'>
         <h1>Welcome to a sneaker collector</h1>
         <p className='py-6'>This tool not only lets you showcase your prized sneaker collection but also provides you with the tools to curate, organize, and catalogue your sneakers
