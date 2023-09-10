80fe79d66f914733a01413a808062b79f3c87433
Maria Filtsova <filtsovamaria@seznam.cz>
2023-09-10 10:46:35 +0200

    fix: prevent minwidth from clipping  drawer items

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -12,7 +12,7 @@ export const Drawer: React.FC<Props> = ({ open, closeHandler, title, children })
     return (
         <>
             <div className={`fixed top-0 left-0 w-full h-full bg-black/60 z-10 ${open ? "visible opacity-100" : "invisible opacity-0"} transition-opacity`}>
-                <div className={`fixed top-0 right-0 h-full z-10 ${open ? "w-4/12 min-w-[300px]" : "w-0"} bg-white transition-all p-12`}>
+                <div className={`fixed top-0 right-0 h-full z-10 ${open ? "w-4/12 min-w-[350px]" : "w-0"} bg-white transition-all p-12`}>
                     <div className='flex'>
                         <h2>{title}</h2>
                         <MdOutlineClose size={24} onClick={closeHandler} className='text-black min-w-[24px]' />
